a5c5fe12fc2a6b0219465e6a91a82774760e62eb
Dirk Pahl <dirk.pahl@web.de>
2020-03-05 15:45:56 +0100

    Add config for private failover IP

--- molecule/debian10/tests/test_keepalived.py
+++ molecule/debian10/tests/test_keepalived.py
@@ -14,6 +14,10 @@ def host_generator():
         yield hostname
 
 
+FAILOVER_PUBLIC_IP = "154.222.4.34"
+FAILOVER_PRIVATE_IP = "192.168.200.250"
+
+
 @ddt
 class RescueModeTest(unittest.TestCase):
 
@@ -42,11 +46,32 @@ class RescueModeTest(unittest.TestCase):
         host_master = testinfra.get_host("docker://keepalived1")
         host_backup = testinfra.get_host("docker://keepalived2")
 
-        master_status_cmd = host_master.run("cat /var/run/keepalived.hetzner_failover.state")
+        state_cmd = "cat /var/run/keepalived.hetzner_failover_ip.state"
+        master_status_cmd = host_master.run(state_cmd)
         master_status = master_status_cmd.stdout.strip()
 
-        backup_status_cmd = host_backup.run("cat /var/run/keepalived.hetzner_failover.state")
+        backup_status_cmd = host_backup.run(state_cmd)
         backup_status = backup_status_cmd.stdout.strip()
 
         assert master_status == "MASTER"
         assert backup_status == "BACKUP"
+
+        pub_ip_cmd = "ip addr | grep '" + FAILOVER_PUBLIC_IP + "' | wc -l"
+        priv_ip_cmd = "ip addr | grep '" + FAILOVER_PRIVATE_IP + "' | wc -l"
+
+        master_pub_ip_cmd = host_master.run(pub_ip_cmd)
+        master_pub_ip_count = int(master_pub_ip_cmd.stdout.strip())
+
+        master_priv_ip_cmd = host_master.run(priv_ip_cmd)
+        master_priv_ip_count = int(master_priv_ip_cmd.stdout.strip())
+
+        backup_pub_ip_cmd = host_backup.run(pub_ip_cmd)
+        backup_pub_ip_count = int(backup_pub_ip_cmd.stdout.strip())
+
+        backup_priv_ip_cmd = host_backup.run(priv_ip_cmd)
+        backup_priv_ip_count = int(backup_priv_ip_cmd.stdout.strip())
+
+        assert master_pub_ip_count == 1
+        assert master_priv_ip_count == 1
+        assert backup_pub_ip_count == 0
+        assert backup_priv_ip_count == 0
