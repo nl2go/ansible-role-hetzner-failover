28c370dc2a8c4a75fbadc003ec83c99aa11f3379
Dirk Pahl <dirk.pahl@web.de>
2020-03-09 18:10:29 +0100

    Use hetznercloud instances for default tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,39 +1,24 @@
 import os
-import unittest
 
 import testinfra.utils.ansible_runner
-from ddt import ddt, idata
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
 ).get_hosts('all')
 
 
-def host_generator():
-    for hostname in testinfra_hosts:
-        yield hostname
+def test_keepalived_config_exists(host):
+    f = host.file('/etc/keepalived/keepalived.conf')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
 
 
-@ddt
-class RescueModeTest(unittest.TestCase):
+def test_keepalived_installed_and_running(host):
+    package = host.package("keepalived")
+    assert package.is_installed
 
-    @idata(host_generator())
-    def test_keepalived_config_exists(self, hostname):
-        host = testinfra.get_host("docker://" + hostname)
-        f = host.file('/etc/keepalived/keepalived.conf')
-
-        assert f.exists
-        assert f.user == 'root'
-        assert f.group == 'root'
-
-    @idata(host_generator())
-    def test_keepalived_installed_and_running(self, hostname):
-        host = testinfra.get_host("docker://" + hostname)
-
-        package = host.package("keepalived")
-        assert package.is_installed
-
-        keepalived_procs_cmd = host.run("pgrep keepalived | wc -l")
-        keepalived_procs = int(keepalived_procs_cmd.stdout.strip())
-
-        assert keepalived_procs > 0
+    service = host.service("keepalived")
+    assert service.is_running
+    assert service.is_enabled
